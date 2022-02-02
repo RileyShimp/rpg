e66d0d328824e76542815f09973b61599925eb0b
Riley Shimp <rilryshimp@gmail.com>
2022-02-02 14:09:23 -0800

    Add everything else
    Co-Authored-by: Andy Plymate <Andy.Plymate@gmail.com>
    Co-authored-by: Jase Seeley <jcseeley31590@yahoo.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -3,16 +3,136 @@ import Character from '../src/js/character.js';
 describe('Character', () => {
 
   let character1;
+  let character2;
+  let character3;
+  let character4;
 
   beforeEach(() => {
     character1 = new Character("monkey")
+    character2 = new Character("cat")
+    character3 = new Character("raccoon")
+    character4 = new Character("dog")
   });
+
 
   test('should create character object', () => {
     expect(character1.type).toEqual("monkey");
   });
+  
 
   test('should increase fullness depending on food that was eaten', () => {
+    character1.eat();
+    expect(character1.fullness).toBeGreaterThan(0);
+  })
+
+  test('should increase fullness depending on food that was eaten', () => {
+    character2.eat();
+    expect(character2.fullness).toBeGreaterThan(0);
+  })
+
+  test('should increase fullness depending on food that was eaten', () => {
+    character3.eat();
+    expect(character3.fullness).toBeGreaterThan(0);
+  })
+
+  test('should increase fullness depending on food that was eaten', () => {
+    character4.eat();
+    expect(character4.fullness).toBeGreaterThanOrEqual(0);
+  })
+
+  test('should increase fullness by 2 if food = taco', () => {
+    character1.food = "taco";
+    character1.monkeyEat();
     expect(character1.fullness).toEqual(2);
+  })
+
+  test('should increase fullness by 3 if food = banana', () => {
+    character1.food = "banana";
+    character1.monkeyEat();
+    expect(character1.fullness).toEqual(3);
+  })
+
+  test('should increase fullness by 4 if food = poo', () => {
+    character1.food = "poo";
+    character1.monkeyEat();
+    expect(character1.fullness).toEqual(4);
+  })
+
+  test('should increase fullness by 3 if food = pizza', () => {
+    character1.food = "pizza";
+    character1.monkeyEat();
+    expect(character1.fullness).toEqual(3);
+  })
+
+  test('should increase fullness by 4 if food = taco', () => {
+    character2.food = "taco";
+    character2.catEat();
+    expect(character2.fullness).toEqual(4);
+  })
+
+  test('should increase fullness by 1 if food = banana', () => {
+    character2.food = "banana";
+    character2.catEat();
+    expect(character2.fullness).toEqual(1);
+  })
+
+  test('should increase fullness by 4 if food = poo', () => {
+    character2.food = "poo";
+    character2.catEat();
+    expect(character2.fullness).toEqual(4);
+  })
+
+  test('should increase fullness by 2 if food = pizza', () => {
+    character2.food = "pizza";
+    character2.catEat();
+    expect(character2.fullness).toEqual(2);
+  })
+
+  test('should increase fullness by 2 if food = taco', () => {
+    character4.food = "taco";
+    character4.dogEat();
+    expect(character4.fullness).toEqual(2);
+  })
+  
+  test('should increase fullness by 0 if food = banana', () => {
+    character4.food = "banana";
+    character4.dogEat();
+    expect(character4.fullness).toEqual(0);
+  })
+  
+  test('should increase fullness by 6 if food = poo', () => {
+    character4.food = "poo";
+    character4.dogEat();
+    expect(character4.fullness).toEqual(6);
+  })
+  
+  test('should increase fullness by 3 if food = pizza', () => {
+    character4.food = "pizza";
+    character4.dogEat();
+    expect(character4.fullness).toEqual(3);
+  })
+  
+  test('should increase fullness by 1 if food = taco', () => {
+    character3.food = "taco";
+    character3.raccoonEat();
+    expect(character3.fullness).toEqual(1);
+  })
+  
+  test('should increase fullness by 1 if food = banana', () => {
+    character3.food = "banana";
+    character3.raccoonEat();
+    expect(character3.fullness).toEqual(1);
+  })
+  
+  test('should increase fullness by 4 if food = poo', () => {
+    character3.food = "poo";
+    character3.raccoonEat();
+    expect(character3.fullness).toEqual(4);
+  })
+  
+  test('should increase fullness by 5 if food = pizza', () => {
+    character3.food = "pizza";
+    character3.raccoonEat();
+    expect(character3.fullness).toEqual(5);
   })
 });
--- src/js/character.js
+++ src/js/character.js
@@ -1,10 +1,84 @@
 export default class Character {
-  constructor(type, fullness) {
+  constructor(type) {
     this.type = type;
     this.fullness = 0;
+    this.foodList = ["banana","taco","poo", "pizza"]
+    this.food = ""
   }
 
   eat() {
+    this.food = this.foodList[(Math.floor(Math.random() * 4))];
+    if(this.type === "monkey"){
+      return this.monkeyEat();
+    }
+    else if(this.type === "cat"){
+      return this.catEat();
+    }
+    else if(this.type === "raccoon"){
+      return this.raccoonEat();
+    }
+    else {
+      return this.dogEat();
+    }
+  }
 
+  monkeyEat() {
+    if (this.food === "taco"){
+      return this.fullness += 2;
+    }
+    else if (this.food === "banana"){
+      return this.fullness += 3;
+    }
+    else if (this.food === "poo"){
+      return this.fullness += 4;
+    }
+    else {
+      return this.fullness += 3;
+    }
+  }
+
+  catEat () {
+    if (this.food === "taco"){
+      this.fullness += 4;
+    }
+    else if (this.food === "banana"){
+      this.fullness += 1;
+    }
+    else if (this.food === "poo"){
+      this.fullness += 4;
+    }
+    else {
+    this.fullness += 2;
+    }
+  }
+
+  raccoonEat() {
+    if (this.food === "taco"){
+      return this.fullness += 1;
+    }
+    else if (this.food === "banana"){
+      return this.fullness += 1;
+    }
+    else if (this.food === "poo"){
+      return this.fullness += 4;
+    }
+    else {
+      return this.fullness += 5;
+    }
+  }
+
+  dogEat() {
+    if (this.food === "taco"){
+      return this.fullness += 2;
+    }
+    else if (this.food === "banana"){
+      return this.fullness += 0;
+    }
+    else if (this.food === "poo"){
+      return this.fullness += 6;
+    }
+    else {
+      return this.fullness += 3;
+    }
   }
 }
